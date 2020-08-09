ee74dfd08f1d2e1e2e606c37450e58e7efdb3979
Olga <aboutrocky94@gmail.com>
2020-08-09 16:06:50 +0300

    - comment

--- src/Main.java
+++ src/Main.java
@@ -21,23 +21,5 @@ public class Main {
                 outputStream2.write(reverse.getBytes());
             }
         }
-
-        //чтение 2 способ
-        /*File file2 = new File("fileReverse.txt");
-        OutputStream os = new FileOutputStream(file2);
-        try (FileReader fr = new FileReader(file);
-             LineNumberReader lnr = new LineNumberReader(fr)) {
-            String line; //считываем строку
-            while ((line = lnr.readLine()) != null) { //вернет null в конце файла
-                System.out.println(line);
-                char[] reverseArray = line.toCharArray();
-                for (int i = reverseArray.length - 1; i >= 0; i--) {
-                    System.out.print(reverseArray[i]);
-                    try (Writer ch = new FileWriter(file2, true)) {
-                        ch.append(reverseArray[i]);
-                    }
-                }
-            }
-        }*/
     }
 }
